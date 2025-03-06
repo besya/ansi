e998eacaeb7cb8be1fcbc6be2b06ce7c4986328c
Ihar Biaspalau <4789696+besya@users.noreply.github.com>
2025-03-06 11:41:05 +0300

    feat: use swift-format-plugin

--- Package.swift
+++ Package.swift
@@ -3,41 +3,6 @@
 
 import PackageDescription
 
-#if os(Windows)
-let plugins: [Target.PluginUsage]? = nil
-#else
-let plugins: [Target.PluginUsage]? = [.plugin(name: "SwiftFormatPlugin")]
-#endif
-
-let target: Target = .target(
-  name: "ANSI",
-  dependencies: [
-    .product(name: "ASCII", package: "ASCII"),
-    .product(name: "Algorithms", package: "swift-algorithms"),
-  ],
-  plugins: plugins
-)
-
-let testTarget: Target = .testTarget(
-  name: "ANSITests",
-  dependencies: ["ANSI"]
-)
-
-#if os(Windows)
-let targets: [Target] = [target, testTarget]
-#else
-let targets: [Target] = [
-  target,
-  testTarget,
-  .plugin(
-    name: "SwiftFormatPlugin",
-    capability: .buildTool(),
-    path: "Plugins/SwiftFormatPlugin",
-    packageAccess: true
-  ),
-]
-#endif
-
 let package = Package(
   name: "ANSI",
   platforms: [.macOS(.v12)],
@@ -47,6 +12,22 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/besya/ascii.git", from: "1.1.0"),
     .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0"),
+    .package(url: "https://github.com/besya/swift-format-plugin.git", from: "0.0.5"),
   ],
-  targets: targets
+  targets: [
+    .target(
+      name: "ANSI",
+      dependencies: [
+        .product(name: "ASCII", package: "ASCII"),
+        .product(name: "Algorithms", package: "swift-algorithms"),
+      ],
+      plugins: [
+        .plugin(name: "LintOnBuild", package: "swift-format-plugin")
+      ]
+    ),
+    .testTarget(
+      name: "ANSITests",
+      dependencies: ["ANSI"]
+    ),
+  ]
 )
