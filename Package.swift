e8c1957d5e233f3f22320c43c0a6bc077dd16b62
Ihar Biaspalau <4789696+besya@users.noreply.github.com>
2025-03-13 12:43:49 +0300

    feat: bump ascii

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     .library(name: "ANSI", targets: ["ANSI"])
   ],
   dependencies: [
-    .package(url: "https://github.com/swiftconsole/ascii.git", from: "1.1.0"),
+    .package(url: "https://github.com/swiftconsole/ascii.git", from: "1.2.0"),
     .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0"),
   ],
   targets: [
